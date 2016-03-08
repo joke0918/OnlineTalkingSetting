f78099dfaf0966a7f8988aea616c755036ab29f3
Yeo Huang <joke0918@163.com>
2016-03-08 20:01:57 +0800

    1

--- OnlineTalking/AppDelegate.swift
+++ OnlineTalking/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-			print("11111112342343")
+			print("32143253441323")
         return true
     }
 
